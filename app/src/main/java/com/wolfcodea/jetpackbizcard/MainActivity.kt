8108f0458ba27ff44ca2263a5dc805b006fc3049
Rodrigo Silva <rodrigoa.work@gmail.com>
2023-08-31 00:18:47 -0300

    adjust card and add image profile and text

--- app/src/main/java/com/wolfcodea/jetpackbizcard/MainActivity.kt
+++ app/src/main/java/com/wolfcodea/jetpackbizcard/MainActivity.kt
@@ -3,21 +3,33 @@ package com.wolfcodea.jetpackbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wolfcodea.jetpackbizcard.ui.theme.JetpackBizCardTheme
@@ -28,7 +40,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackBizCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     CreateBizCard()
                 }
             }
@@ -37,23 +52,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateBizCard(){
-    Surface (modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()){
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
 
-    Card(
-        modifier = Modifier.width(200.dp).height(390.dp).padding(16.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.Black,
-        ),
-        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 10.dp
-        ))
-    {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        )
+        {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                CreateImageProfile()
+                Spacer(Modifier.size(20.dp))
+                Divider(
+                    thickness = 2.dp,
+                    color = Color.LightGray,
+                )
+                Column(modifier = Modifier.padding(5.dp)) {
+                    Text(
+                        text = "Rodrigo S.",
+                        style = MaterialTheme.typography.displaySmall,
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                    )
+                }
+            }
 
+
+        }
     }
+}
+
+@Composable
+private fun CreateImageProfile() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.img_profile),
+            contentDescription = "Image Profile",
+            modifier = Modifier.size(135.dp),
+        )
     }
 }
 
